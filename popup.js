3fb748325abf722a80a9f0b0b79b322e289315bd
Christopher Partridge <cpartridge@Christophers-MacBook-Air-3.local>
2020-01-25 15:18:28 -0500

    Popup not working?

--- popup.js
+++ popup.js
@@ -1,20 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
 
+d = document;
+
+d.addEventListener('DOMContentLoaded', function() {
+  var fillPagebutton = d.getElementById('autofill');
+
+  fillPagebutton.addEventListener('click', function() {
     chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+      var inputs=d.getElementsByTagName("input");    //look for all inputs
+      var fields = ['first_name', 'last_name', 'email'];
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
+      print("runs");
+      for(var i = 0; i < inputs.length; i++){   //for each input on document
+
+            var input=inputs[i];     //look at whatever input
+
+            if(input.type=="text" && (fields.indexOf(input.id)!=-1)){
+                d.getElementById(inputs[i].id).value="TEST";
+                //d.forms[0].submit();
+            }
+       }});
     });
-  }, false);
-}, false);
+      
+  });
+ 
+
