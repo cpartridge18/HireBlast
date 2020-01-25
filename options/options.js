961e3b5e54ad1393dc9e1b8d911233b784602827
Warren-Partridge <maps@perdix.org>
2020-01-25 18:32:38 -0500

    Make options save and retrieve user info correctly

--- options/options.js
+++ options/options.js
@@ -1,23 +1,39 @@
-function saveOptions() {
-  var firstName = document.getElementById("first-name").value;
-  var lastName = document.getElementById("last-name").value;
+document.addEventListener('DOMContentLoaded', function() {
+  function saveOptions() {
+    var firstName = document.getElementById("first-name").value;
+    var lastName = document.getElementById("last-name").value;
 
-  var prefs = {
-    firstName: firstName,
-    lastName: lastName
+    var prefs = {
+      firstName: firstName,
+      lastName: lastName
+    }
+
+    console.log(prefs);
+
+    chrome.storage.sync.set({"hbPrefs": prefs}, function() {
+      restore_options();
+      document.getElementById('save-confirmation').innerHTML = "Saved!";
+
+      setTimeout(function() {
+        document.getElementById('save-confirmation').innerHTML = "";
+      }, 1000);
+
+    });
+
   }
 
-  console.log(prefs);
 
-  chrome.storage.sync.set({hbPrefs: prefs}, () => {console.log('Saved!', prefs);});
-}
+  function restore_options() {
+    chrome.storage.sync.get(["hbPrefs"], function(prefs) {
+      document.getElementById('first-name').placeholder = prefs.hbPrefs.firstName;
+      document.getElementById('first-name').value = "";
+      document.getElementById('last-name').placeholder = prefs.hbPrefs.lastName;
+      document.getElementById('last-name').value = "";
+    });
+  }
 
 
+  restore_options();
 
-document.addEventListener('DOMContentLoaded', function() {
-  var link = document.getElementById('save-button');
-  // onClick's logic below:
-  link.addEventListener('click', function() {
-    saveOptions();
-  });
+  document.getElementById('save-button').addEventListener('click', saveOptions);
 });
