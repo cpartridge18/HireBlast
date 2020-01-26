beef4d0b974bc40c7f1998d12e4be8d00928c019
Warren-Partridge <maps@perdix.org>
2020-01-25 20:18:07 -0500

    Stub out autofilling from user options

--- contentScript.js
+++ contentScript.js
@@ -7,18 +7,26 @@ function gotMessage(message, sender, sendResponse){
   console.log(message)
   if (message.txt === "hello") {
     var inputs=d.getElementsByTagName("input");    //look for all inputs
-       var fields = ['first_name', 'last_name', 'email'];
+       var fields = ['first_name', 'first-name', 'last_name', 'email'];
 
-//       print("runs");
-      for(var i = 0; i < inputs.length; i++){   //for each input on document
+       // {"firstName": ["first_name", "first-name"]}
 
-            var input=inputs[i];     //look at whatever input
+      chrome.storage.sync.get(["hbPrefs"], function(prefs) {
+        var ZZZZZZ = prefs.hbPrefs.firstName;
+        console.log(prefs);
 
-            if(input.type=="text" && (fields.indexOf(input.id)!=-1)){
-                d.getElementById(inputs[i].id).value="TEST";
-                //d.forms[0].submit();
-            }
+        for(var i = 0; i < inputs.length; i++){   //for each input on document
+
+          var input=inputs[i];     //look at whatever input
+
+
+          if(input.type=="text" && (fields.indexOf(input.id)!=-1)){
+            d.getElementById(inputs[i].id).value=ZZZZZZ;
+            //d.forms[0].submit();
           }
+        }
+      });
+
   }
 }
 // chrome.tabs.getSelected(null, function(tab) {
